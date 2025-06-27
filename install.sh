2abd0c6bdbe0982e2f16069a1fd65b85bd209ac0
jimno <98581961+DerJimno@users.noreply.github.com>
2025-06-27 14:32:52 +0100

    test

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+loadkeys us
+ping google.com
+timedatectl set-ntp true
+timedatectl status
+
 DEVICE="/dev/sda"
 
 # Check before proceeding
@@ -28,3 +33,42 @@ w
 EOF
 
 
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/sda2
+
+mount /dev/sda2 /mnt 
+
+pacstrap /mnt base linux llinux-firmware neovim sudo grub efibootmgr dosfstools os-prober mtools networkmanager git 
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
+ln -sf /usr/share/zoneinfo/Africa/Algiers /etc/localtime
+
+hwclock --systohc
+
+# Locale
+nvim -c 'g/^#\s*en_US.UTF/s/^#\s*//' -c 'wq' /etc/locale.gen
+locale-gen
+
+echo "pc-box" | tee /etc/hostname
+echo "127.0.1.1	pc-box.localdomain pc-box" | /etc/hosts
+
+passwd
+useradd -m jimno 
+passwd jimno
+
+EDITOR='nvim +":g/^#\s*%wheel\s\+ALL/s/^#\s*//" +wq' visudo
+ 
+sudo groupadd uinput
+usermod -aG wheel,audio,video,optical,storage,input,uinput jimno
+
+mkdir /boot/EFI
+mount /dev/sda1 /boot/EFI
+grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+systemctl enable NetworkManager
+
+
+exit 
+umount -l /mnt
